e247a7adb2acf1b91fa9f1d1f35f7e33798e3ce4
Cyril <cy_one2000@yahoo.fr>
2022-08-20 19:03:32 +0200

    functions getComputerChoice getResult onClickRPS playGame

--- script.js
+++ script.js
@@ -14,7 +14,9 @@
 // getComputerChoice() 👉 'Rock'
 // getComputerChoice() 👉 'Scissors'
 function getComputerChoice() {
-  
+  const shifumiChoix = ['Rock', 'Paper', 'Scissors']
+  const randomChoixOrdi = Math.floor(Math.random() * 3)
+  return shifumiChoix[randomChoixOrdi]
 }
 
 // ** getResult compares playerChoice & computerChoice and returns the score accordingly **
@@ -23,22 +25,25 @@ function getComputerChoice() {
 // human draws - getResult('Rock', 'Rock') 👉 0
 function getResult(playerChoice, computerChoice) {
   // return the result of score based on if you won, drew, or lost
-    
-    
-  
+  let score
   // All situations where human draws, set `score` to 0
-    
-  
-  // All situations where human wins, set `score` to 1
-  // make sure to use else ifs here
-    
-  
-  // Otherwise human loses (aka set score to -1)
-    
-  
+  if (playerChoice == computerChoice) {
+    score = 0
+    // All situations where human wins, set `score` to 1
+  } else if (playerChoice == 'rock ' && computerChoice == 'scissors') {
+    score = 1
+  } else if (playerChoice == 'paper ' && computerChoice == 'rock') {
+    score = 1
+  } else if (playerChoice == 'scissors ' && computerChoice == 'paper') {
+    score = 1
+    // Otherwise human loses (aka set score to -1)
+  } else {
+    score = -1
+  }
   // return score
-    
+  return score
 }
+
 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice) {
@@ -49,30 +54,38 @@ function showResult(score, playerChoice, computerChoice) {
 
 // ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
-  
+  console.log({ playerChoice })
+  const computerChoice = getComputerChoice()
+  console.log({ computerChoice })
+  const score = getResult(playerChoice, computerChoice)
+  console.log({ score })
 }
 
 
 // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
   // use querySelector to select all RPS Buttons
-  
+  const rpsButtons = document.querySelectorAll('.rpsButton')
+  console.log(rpsButtons);
+  rpsButtons[0].onclick = () => console.log(rpsButtons[0].value)
+
   // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
-    
+
   // 1. loop through the buttons using a forEach loop
   // 2. Add a 'click' event listener to each button
   // 3. Call the onClickRPS function every time someone clicks
   // 4. Make sure to pass the currently selected rps button as an argument
-  
-   
-  
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton.value)
+  })
+
   // Add a click listener to the end game button that runs the endGame() function on click
-    
+
 }
 
 // ** endGame function clears all the text on the DOM **
 function endGame() {
-  
+
 }
 
 playGame()
